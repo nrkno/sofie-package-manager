d9c4d86cb25a54ec69daa0204f80755285b96c84
Johan Nyman <johan@nytamin.se>
2022-01-19 13:15:41 +0100

    chore: check if signtool is installed before signing executables

--- scripts/sign-executables.js
+++ scripts/sign-executables.js
@@ -1,7 +1,7 @@
-const promisify = require("util").promisify
-const glob = promisify(require("glob"))
-const { exec } = require("child_process");
-const readline = require('readline');
+const promisify = require('util').promisify
+const glob = promisify(require('glob'))
+const { exec } = require('child_process')
+const readline = require('readline')
 
 const execPromise = promisify(exec)
 
@@ -13,55 +13,66 @@ const folderPath = 'deploy/'
 const certificatePath = '.'
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+	input: process.stdin,
+	output: process.stdout,
+})
 
 ;(async function () {
+	let signtoolInstalled = false
+	try {
+		await execPromise('where signtool')
+		signtoolInstalled = true
+	} catch (e) {
+		signtoolInstalled = false
+	}
 
+	if (!signtoolInstalled) {
+		console.log(
+			'Warning: signtool is not installed. To enable signing of the resulting executables, install the Microsoft SDK for Windows 10 and add signtool.exe to your PATH.'
+		)
+		return
+	}
 
-    const executables = await glob(`${folderPath}/*.exe`)
+	const executables = await glob(`${folderPath}/*.exe`)
 
-    const certificates = await glob(`${certificatePath}/*.pfx`)
-    
+	const certificates = await glob(`${certificatePath}/*.pfx`)
 
-    if (certificates.length === 0) {
-        console.log('No certificates found. To sign the resulting executables, add one or more certificates (*.pfx file) to the base folder.')
-        return
-    }
-    if (executables.length === 0) {
-        console.log(`No executables found in ${folderPath}`)
-        return
-    }
+	if (certificates.length === 0) {
+		console.log(
+			'Warning: No certificates found. To sign the resulting executables, add one or more certificates (*.pfx file) to the base folder.'
+		)
+		return
+	}
+	if (executables.length === 0) {
+		console.log(`Warning: No executables found in ${folderPath}`)
+		return
+	}
 
-    console.log(`Found ${executables.length} executables`)
-    console.log(`Found ${certificates.length} certificates`)
+	console.log(`Found ${executables.length} executables`)
+	console.log(`Found ${certificates.length} certificates`)
 
-    for (const certificate of certificates) {
+	for (const certificate of certificates) {
+		console.log(`Signing with certificate ${certificate}...`)
 
-        console.log(`Signing with certificate ${certificate}...`)
-    
-    
-        const password = await new Promise((resolve) => {
-            rl.question('Enter password for certificate: ', resolve)
-        })
-    
-        for (const executable of executables) {
-            const e = await execPromise(`signtool sign /fd SHA256 /f ${certificate} ${password ? `/p ${password} ` : ''} ${executable}`)
-            if (e.stderr) console.log(e.stderr)
-            if (e.stdout) console.log(e.stdout)
-        }
-    }
+		const password = await new Promise((resolve) => {
+			rl.question('Enter password for certificate: ', resolve)
+		})
 
-    
-    
+		for (const executable of executables) {
+			const e = await execPromise(
+				`signtool sign /fd SHA256 /f ${certificate} ${password ? `/p ${password} ` : ''} ${executable}`
+			)
+			if (e.stderr) console.log(e.stderr)
+			if (e.stdout) console.log(e.stdout)
+		}
+	}
 
 	console.log(`Done, signed ${executables.length} executables.`)
 })()
-.then(() => {
-    process.exit(0)
-})
-.catch((e) => {
-    console.error(e)
-    process.exit(1)
-})
+	.then(() => {
+		process.exit(0)
+	})
+	.catch((e) => {
+		console.error(e)
+		process.exit(1)
+	})
