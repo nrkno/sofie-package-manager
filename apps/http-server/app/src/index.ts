723254da345d981ea0db62bdc3d39b351f3a8a68
Jan Starzak <jan.starzak@gmail.com>
2023-06-16 17:12:20 +0200

    experiment: see if the problem is resolving the `postPackage` promise too early

--- apps/http-server/app/src/index.ts
+++ apps/http-server/app/src/index.ts
@@ -1,5 +1,8 @@
 import { startProcess } from '@http-server/generic'
 /* eslint-disable no-console */
 
+import packageInfo from './../package.json'
+
 console.log('process started') // This is a message all Sofie processes log upon startup
+console.log(`version: ${packageInfo.version}`)
 startProcess().catch(console.error)
--- apps/single-app/app/src/index.ts
+++ apps/single-app/app/src/index.ts
@@ -1,6 +1,10 @@
 import { startSingleApp } from './singleApp'
 
+import packageInfo from './../package.json'
+
 // eslint-disable-next-line no-console
 console.log('process started') // This is a message all Sofie processes log upon startup
+// eslint-disable-next-line no-console
+console.log(`version: ${packageInfo.version}`)
 // eslint-disable-next-line no-console
 startSingleApp().catch(console.error)
