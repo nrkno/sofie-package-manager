ac0cd4684a5709dfbb6e1eda268d42e446d67967
Jan Starzak <jan.starzak@gmail.com>
2023-06-19 13:27:35 +0200

    fix: remove version reporting

--- apps/http-server/app/src/index.ts
+++ apps/http-server/app/src/index.ts
@@ -1,10 +1,5 @@
 import { startProcess } from '@http-server/generic'
-import fs from 'fs'
-import path from 'path'
 /* eslint-disable no-console */
 
-const packageInfo = JSON.parse(fs.readFileSync(path.join(__dirname, './../package.json')).toString('utf-8'))
-
 console.log('process started') // This is a message all Sofie processes log upon startup
-console.log(`version: ${packageInfo.version}`)
 startProcess().catch(console.error)
--- apps/single-app/app/src/index.ts
+++ apps/single-app/app/src/index.ts
@@ -1,12 +1,6 @@
 import { startSingleApp } from './singleApp'
-import fs from 'fs'
-import path from 'path'
-
-const packageInfo = JSON.parse(fs.readFileSync(path.join(__dirname, './../package.json')).toString('utf-8'))
 
 // eslint-disable-next-line no-console
 console.log('process started') // This is a message all Sofie processes log upon startup
-// eslint-disable-next-line no-console
-console.log(`version: ${packageInfo.version}`)
 // eslint-disable-next-line no-console
 startSingleApp().catch(console.error)
