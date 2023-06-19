c546b5fd6457e7b30381f28e4f802b4f385a3348
Jan Starzak <jan.starzak@gmail.com>
2023-06-19 13:16:21 +0200

    fix: package.json asset build in pkg

--- apps/single-app/app/src/index.ts
+++ apps/single-app/app/src/index.ts
@@ -1,8 +1,8 @@
 import { startSingleApp } from './singleApp'
-import { readFileSync } from 'fs'
-import { join as pathJoin } from 'path'
+import fs from 'fs'
+import path from 'path'
 
-const packageInfo = JSON.parse(readFileSync(pathJoin(__dirname, './../package.json')).toString('utf-8'))
+const packageInfo = JSON.parse(fs.readFileSync(path.join(__dirname, './../package.json')).toString('utf-8'))
 
 // eslint-disable-next-line no-console
 console.log('process started') // This is a message all Sofie processes log upon startup
