07443a774b71112020ca0bc31e8f9bf3a7d41c94
Johan Nyman <johan@nytamin.se>
2023-10-12 08:27:08 +0200

    chore: refactor: rename

--- shared/packages/expectationManager/src/internalManager/lib/managerStatusWatchdog.ts
+++ shared/packages/expectationManager/src/internalManager/lib/managerStatusWatchdog.ts
@@ -3,11 +3,11 @@ import { ExpectationTracker } from '../../expectationTracker/expectationTracker'
 import { ManagerStatusReporter } from './managerStatusReporter'
 
 /** Monitors the status of the ExpectationTracker and alerts if there's a problem */
-export class ManagerStatusMonitor {
-	private statusMonitorInterval: NodeJS.Timeout | null = null
+export class ManagerStatusWatchdog {
+	private checkStatusInterval: NodeJS.Timeout | null = null
 
 	/** Timestamp, used to determine how long the work-queue has been stuck */
-	private monitorStatusWaiting: number | null = null
+	private stuckTimestamp: number | null = null
 
 	private logger: LoggerInstance
 	constructor(
@@ -17,32 +17,32 @@ export class ManagerStatusMonitor {
 	) {
 		this.logger = logger.category('StatusMonitor')
 
-		this.statusMonitorInterval = setInterval(() => {
-			this._monitorStatus()
+		this.checkStatusInterval = setInterval(() => {
+			this._checkStatus()
 		}, 60 * 1000)
 	}
 
 	public terminate(): void {
-		if (this.statusMonitorInterval) {
-			clearInterval(this.statusMonitorInterval)
-			this.statusMonitorInterval = null
+		if (this.checkStatusInterval) {
+			clearInterval(this.checkStatusInterval)
+			this.checkStatusInterval = null
 		}
 	}
 
-	private _monitorStatus() {
+	private _checkStatus() {
 		// If the work-queue is long (>10 items) and nothing has progressed for the past 10 minutes.
 
 		const waitingExpectationCount = this.tracker.scaler.getWaitingExpectationCount()
 
 		if (waitingExpectationCount > 10) {
-			if (!this.monitorStatusWaiting) {
-				this.monitorStatusWaiting = Date.now()
+			if (!this.stuckTimestamp) {
+				this.stuckTimestamp = Date.now()
 			}
 		} else {
-			this.monitorStatusWaiting = null
+			this.stuckTimestamp = null
 		}
 
-		const stuckDuration: number = this.monitorStatusWaiting ? Date.now() - this.monitorStatusWaiting : 0
+		const stuckDuration: number = this.stuckTimestamp ? Date.now() - this.stuckTimestamp : 0
 		if (stuckDuration > 10 * 60 * 1000) {
 			this.logger.error(`_monitorStatus: Work Queue is Stuck for ${stuckDuration / 1000 / 60} minutes`)
 			this.managerStatuses.update('work-queue-stuck', {
