7d36627323faa030d6bec0506ab4a416b3e49d5d
Johan Nyman <johan@nytamin.se>
2024-07-11 14:54:13 +0200

    fix: fix issue in HelpfulEventEmitter where it logged false errors

--- shared/packages/api/src/HelpfulEventEmitter.ts
+++ shared/packages/api/src/HelpfulEventEmitter.ts
@@ -10,28 +10,31 @@ export class HelpfulEventEmitter extends EventEmitter {
 		// Ensure that the error event is listened for:
 
 		const orgError = new Error('No error event listener registered')
-		setTimeout(() => {
-			for (const event of this._listenersToCheck) {
-				if (!this.listenerCount(event)) {
-					// If no event listener is registered, log a warning to let the developer
-					// know that they should do so:
-					console.error(`WARNING: No "${event}" event listener registered`)
-					console.error(`Stack: ${orgError.stack}`)
 
-					// If we're running in Jest, it's better to make it a little more obvious that something is wrong:
-					if (process.env.JEST_WORKER_ID !== undefined) {
-						// Since no error listener is registered, this'll cause the process to exit and tests to fail:
-						this.emit('error', orgError)
+		setImmediate(() => {
+			setImmediate(() => {
+				for (const event of this._listenersToCheck) {
+					if (!this.listenerCount(event)) {
+						// If no event listener is registered, log a warning to let the developer
+						// know that they should do so:
+						console.error(`WARNING: No "${event}" event listener registered`)
+						console.error(`Stack: ${orgError.stack}`)
+
+						// If we're running in Jest, it's better to make it a little more obvious that something is wrong:
+						if (process.env.JEST_WORKER_ID !== undefined) {
+							// Since no error listener is registered, this'll cause the process to exit and tests to fail:
+							this.emit('error', orgError)
+						}
 					}
 				}
-			}
 
-			// If we're running in Jest, it's better to make it a little more obvious that something is wrong:
-			if (process.env.JEST_WORKER_ID !== undefined && !this.listenerCount('error')) {
-				// Since no error listener is registered, this'll cause the process to exit and tests to fail:
-				this.emit('error', orgError)
-			}
-		}, 1)
+				// If we're running in Jest, it's better to make it a little more obvious that something is wrong:
+				if (process.env.JEST_WORKER_ID !== undefined && !this.listenerCount('error')) {
+					// Since no error listener is registered, this'll cause the process to exit and tests to fail:
+					this.emit('error', orgError)
+				}
+			})
+		})
 	}
 
 	/**
